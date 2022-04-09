7bb3bfa4589ebc7254ee0225f9e6ebc656fbda7a
Rishi Sadanandan <rishikesh.sadanandan@gmail.com>
2022-04-09 15:18:28 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./Recipe";
 
-function App() {
+const App = () => {
+  // get keys
+  const APP_ID = "1e2946d5";
+  const APP_KEY = "21df6842d0a6a134cd5fb204ef688e55";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]); // only runs when query changes
+
+  // takes care of fetching data
+  const getRecipes = async () => {
+    // once the reponse comes back, we will await, then fetch.
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    // after the repsone comes back, turn it into a json.
+    // need await for every promise because we never know when it will come back from the api.
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+    // we could have use .then promises, but this looks better.
+  };
+
+  const updateSearch = (e) => {
+    // having trouble figuring out what the event and its target actually are.
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  // every time we submit the form, we want to get our search.
+  const getSearch = (e) => {
+    e.preventDefault(); // stops page from reloading
+    setQuery(search);
+    setSearch(""); // visually appealing and logical
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form" onSubmit={getSearch}>
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        ></input>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {recipes.map((recipe) => (
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
